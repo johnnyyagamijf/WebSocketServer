f76a7e7283410bd599a53aa89b0ab5ca6affc839
John Campos <johnmdcampos@outlook.com>
2020-07-08 09:45:54 -0300

    'pc'

---
+++ WebSocketServer/WebServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Fleck;
+namespace WebSocketServer
+{
+    public  class WebServer
+    {
+        public void SendData()
+        {
+            var server = new Fleck.WebSocketServer("ws://127.0.0.1:8081/");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                };
+
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine(message);
+                    if (message.StartsWith("Venda"))
+                    {
+                        string[] dados = message.Split('|');
+
+                        //abrir conexão com o banco de dados SQL.
+                        //Verificar o pedido 
+
+                        //Resposta para o cliente
+                        socket.Send("Confirmado");
+                    }
+                };
+
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                };
+
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
